86b8bb12fb985a754f745bfa4b6f0d41b04a12be
muyangkkong <137609813+muyangkkong@users.noreply.github.com>
2024-11-09 23:39:19 +0900

    Update views.py

--- main/views.py
+++ main/views.py
@@ -8,7 +8,7 @@ from rest_framework.mixins import (
     UpdateModelMixin,
     DestroyModelMixin,
 )
-
+from main.serializers import StudentSerializer
 
 class StudentListAPIView(ListModelMixin, CreateModelMixin, GenericAPIView):
     """
